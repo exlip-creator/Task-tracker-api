79f4f6c1af51df40b33405932c64cd6fd04ff063
exlip-creator <demyan_permyakov@bk.ru>
2026-08-16 17:25:40 +0900

    Described terraform configuration for the Yandex Cloud

---
+++ terraform/security.tf
@@ -0,0 +1,31 @@
+resource "yandex_vpc_security_group" "server" {
+    name = "pet-project-server-sg"
+    network_id = yandex_vpc_network.main.id
+
+    ingress {
+        protocol = "TCP"
+        description = "SSH"
+        port = 22
+        v4_cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        protocol = "TCP"
+        description = "HTTP"
+        port = 80
+        v4_cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        protocol = "TCP"
+        description = "HTTPS"
+        port = 443
+        v4_cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        protocol = "ANY"
+        description = "Allow all outbound traffic"
+        v4_cidr_blocks = ["0.0.0.0/0"]
+    }
+}
